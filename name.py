f118d05f636144d357af797035c1051af94c03ff
ZenG-8man <50492933+ZenG-8man@users.noreply.github.com>
2020-01-02 17:07:02 +0530

    Update name.py

--- name.py
+++ name.py
@@ -2,12 +2,14 @@ def main():
     name = input("What is your name? ")
     work(name)
 
-
 def work(name):
     if name.strip().lower() == 'a':
+        #strip() removes leading and trailing spaces
+        #strip("abc") removes substring
         print("Hello old friend!")
     else:
         print(f"Nice to meet you {name}.")
+        #print(f"") formats string
         print("My name is Python!")
         
 main()
